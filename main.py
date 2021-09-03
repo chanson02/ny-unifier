991811f816761bb347e6a514831e6cfa3fdf8663
chanson <cooperhanson2020@gmail.com>
2021-09-02 22:59:48 -0500

    commented out old logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os, json, csv, psycopg2, re, pdb, time, requests
+from container_manager import ContainerManager
 from drive_downloader import DriveDownloader
 from busybody_getter import BusybodyGetter
 from xlsx_handler import Handler
@@ -36,89 +37,91 @@ def new(drive_time):
         return True
     else:
         return False
+#
+# # function to generate row
+# def gen_row(customer, source, address, product):
+#     return [
+#         source,
+#         customer.name,
+#         address['street'],
+#         address['city'],
+#         address['state'],
+#         address['zip'],
+#         address['phone'],
+#         product,
+#         customer.premise,
+#         customer.website
+#     ]
+#
+# def address_from_row(row):
+#     return {
+#         'street': row[2],
+#         'city': row[3],
+#         'state': row[4],
+#         'zip': row[5],
+#         'phone': row[6]
+#     }
+#
+# # Function to 'merge' csv files
+# def append_csv(to, frm): #from -> to
+#     update_file = csv.writer(open(to, 'a'))
+#
+#     try:
+#         # rows = pd. read_csv(frm).values
+#         rows = pd.ExcelFile(frm).parse().values
+#     except pd.errors.EmptyDataError:
+#         # No unknowns
+#         return
+#     for row in rows:
+#         row = clean(row)
+#         address = address_from_row(row)
+#         if known(address):
+#             update_address_book(row[1], address)
+#         sources = row[0][1:-1].split(',')
+#         if row[7] == '':
+#             # VA Removed the data for some reason?
+#             products = ['' for _ in range(len(sources))]
+#         else:
+#             products = row[7][1:-1].split(',')
+#         for ndx in range(len(sources)):
+#             row[0] = sources[ndx][1:-1]
+#             row[7] = products[ndx][1:-1]
+#             update_file.writerow(row)
+#     return
+#
+# # Function to clean the data from a parsed file
+# def clean(row):
+#     row = [str(v) for v in row]
+#     for i in range(len(row)):
+#         if row[i] == 'nan':
+#             row[i] = ''
+#         row[i] = row[i].strip()
+#     # while len(row) < 10:
+#     #     row.append('')
+#     return row
+#
+# # Function to update the address book with new known customer
+# def update_address_book(customer, address):
+#     customer = customer.lower()
+#     with open('address_book.json', 'r') as f:
+#         address_book = json.load(f)
+#     customers = list(address_book.keys())
+#     if customer not in customers:
+#         address_book[customer] = []
+#     address_book[customer].append(address)
+#     with open('address_book.json', 'w') as f:
+#         json.dump(address_book, f, indent=2)
+#     return
 
-# Function to check if all address data is found
-def known(address):
-    rk = ['street', 'city', 'state', 'zip']
-    missing = [k for k in rk if address[k] == '']
-    return True if len(missing) == 0 else False
-
-# function to generate row
-def gen_row(customer, source, address, product):
-    return [
-        source,
-        customer.name,
-        address['street'],
-        address['city'],
-        address['state'],
-        address['zip'],
-        address['phone'],
-        product,
-        customer.premise,
-        customer.website
-    ]
-
-def address_from_row(row):
-    return {
-        'street': row[2],
-        'city': row[3],
-        'state': row[4],
-        'zip': row[5],
-        'phone': row[6]
-    }
-
-# Function to 'merge' csv files
-def append_csv(to, frm): #from -> to
-    update_file = csv.writer(open(to, 'a'))
-
+# Function to find important subdirectories from brand directory
+def find_io_folders(container_folder):
     try:
-        # rows = pd. read_csv(frm).values
-        rows = pd.ExcelFile(frm).parse().values
-    except pd.errors.EmptyDataError:
-        # No unknowns
-        return
-    for row in rows:
-        row = clean(row)
-        address = address_from_row(row)
-        if known(address):
-            update_address_book(row[1], address)
-        sources = row[0][1:-1].split(',')
-        if row[7] == '':
-            # VA Removed the data for some reason?
-            products = ['' for _ in range(len(sources))]
-        else:
-            products = row[7][1:-1].split(',')
-        for ndx in range(len(sources)):
-            row[0] = sources[ndx][1:-1]
-            row[7] = products[ndx][1:-1]
-            update_file.writerow(row)
-    return
-
-# Function to clean the data from a parsed file
-def clean(row):
-    row = [str(v) for v in row]
-    for i in range(len(row)):
-        if row[i] == 'nan':
-            row[i] = ''
-        row[i] = row[i].strip()
-    # while len(row) < 10:
-    #     row.append('')
-    return row
-
-# Function to update the address book with new known customer
-def update_address_book(customer, address):
-    customer = customer.lower()
-    with open('address_book.json', 'r') as f:
-        address_book = json.load(f)
-    customers = list(address_book.keys())
-    if customer not in customers:
-        address_book[customer] = []
-    address_book[customer].append(address)
-    with open('address_book.json', 'w') as f:
-        json.dump(address_book, f, indent=2)
-    return
-
-
+        unifier_io = [f for f in container_folder if 'unifier_io' in f.path][0]
+        brand_io = [f for f in container_folder if 'brand_io' in f.path][0]
+        return unifier_io, brand_io
+    except IndexError:
+        # No children means new brand
+        return None, None
 
 
 dl = DriveDownloader()
@@ -135,25 +138,26 @@ def search_brands(id):
             return brand
     return None
 
-
+# Runs through each brand folder to find new files
+# adds them to their respective queue
 for brand in brands:
-    try:
-        unifier_io = [f for f in brand.children if 'unifier_io' in f.path][0]
-        brand_io = [f for f in brand.children if 'brand_io' in f.path][0]
-    except IndexError:
-        # No children means new brand
+    unifier_io, brand_io = find_io_folders(brand.children)
+
+    if unifier_io is None: #Initialize new brand
         dl.initialize_brand(brand)
         slack(f'Initializing {brand.folder_data["name"]}') if SLACK else False
         continue
 
-    # Check to see if there is new files
-    for container in brand_io.children:
-        if new(container.folder_data['createdTime']):
-            files_present_queue.append(container)
-
-    for file in unifier_io.files:
-        if new(file['createdTime']) and '_complete' in file['name']:
-            unknowns_learned_queue.append(file)
+    [ # Get new container folders
+        files_present_queue.append(container)
+        for container in brand_io.children
+        if new(container.folder_data['createdTime'])
+    ]
+    [ # Get new complete files
+        unknowns_learned_queue.append(file)
+        for file in unifier_io.files
+        if new(file['createdTime']) and '_complete' in file['name']
+    ]
 print('New files flagged')
 
 # Files Present
@@ -165,31 +169,19 @@ for container in files_present_queue:
     for f in container.files:
         downloads.append(dl.download_file(f))
         print('.', end='', flush=True)
+    print()
+    container_manager = ContainerManager(downloads, container)
+    pdb.set_trace()
 
-    # Setup known and unknown files
-    path = container.path.replace('/', '|')
-    unknown_path = f'./tmp/{path}_incomplete.csv'
-    known_file = csv.writer(open(f'./pending/{path}.csv', 'w'))
-    unknown_file = csv.writer(open(unknown_path, 'w'))
-    known_file.writerow(header)
-    unknown_file.writerow(header)
 
-    # Get customer data by parsing XLSX
-    print(f'\nParsing {len(downloads)} files')
-    customers = []
-    for download in downloads:
-        print('.', end='', flush=True)
-        Handler(download, customers)
 
-    print(f'\nConsulting Busybody on {len(customers)} customers')
-    bbg = BusybodyGetter()
-    # Merge addresses to find best data
-    [c.execute(bbg) for c in customers]
-    bbg.conn.close()
-
-    with open('test.json', 'w') as f:
-        import json
-        json.dump(bbg.failures, f, indent=2)
+    # # Setup known and unknown files
+    # path = container.path.replace('/', '|')
+    # unknown_path = f'./tmp/{path}_incomplete.csv'
+    # known_file = csv.writer(open(f'./pending/{path}.csv', 'w'))
+    # unknown_file = csv.writer(open(unknown_path, 'w'))
+    # known_file.writerow(header)
+    # unknown_file.writerow(header)
 
     print('\nWriting files')
     # Write to csv files
@@ -234,31 +226,31 @@ for container in files_present_queue:
     os.remove(unknown_path)
     print('finished', container)
 
-# Unknowns Completed
-for drive_file in unknowns_learned_queue:
-    # Download file
-    dl.clear_storage()
-    print('downloading', drive_file['name'])
-    csv_path = dl.download_file(drive_file)
-    csv_name = csv_path.split('/')[-1].split('_complete')[0]
-
-    # Match to existing
-    unifier_id = drive_file['parents'][0]
-    brand = search_brands(unifier_id)
-    brand_name = brand.path.split('/')[-1]
-    pending_file_path = [f'./pending/{file}' for file in os.listdir('./pending/') if brand_name in file and csv_name in file][0]
-
-    # Fill out missing info
-    append_csv(pending_file_path, csv_path)
-
-    # Upload as '_finished.csv'
-    complete_file_path = pending_file_path.replace('.csv', '_finished.csv')
-    os.rename(pending_file_path, complete_file_path)
-    dl.upload_file(complete_file_path, unifier_id)
-
-    # Delete local file
-    os.remove(complete_file_path)
-    slack(f'uploaded {brand.path}/unifier_io/{complete_file_path.split("/")[-1]}') if SLACK else False
+# # Unknowns Completed
+# for drive_file in unknowns_learned_queue:
+#     # Download file
+#     dl.clear_storage()
+#     print('downloading', drive_file['name'])
+#     csv_path = dl.download_file(drive_file)
+#     csv_name = csv_path.split('/')[-1].split('_complete')[0]
+#
+#     # Match to existing
+#     unifier_id = drive_file['parents'][0]
+#     brand = search_brands(unifier_id)
+#     brand_name = brand.path.split('/')[-1]
+#     pending_file_path = [f'./pending/{file}' for file in os.listdir('./pending/') if brand_name in file and csv_name in file][0]
+#
+#     # Fill out missing info
+#     append_csv(pending_file_path, csv_path)
+#
+#     # Upload as '_finished.csv'
+#     complete_file_path = pending_file_path.replace('.csv', '_finished.csv')
+#     os.rename(pending_file_path, complete_file_path)
+#     dl.upload_file(complete_file_path, unifier_id)
+#
+#     # Delete local file
+#     os.remove(complete_file_path)
+#     slack(f'uploaded {brand.path}/unifier_io/{complete_file_path.split("/")[-1]}') if SLACK else False
 
 
 print('Continue to record time')
