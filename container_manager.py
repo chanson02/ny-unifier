a1feb8d6dab210e66a4e4ee7384030a6e0b6b58f
chanson <cooperhanson2020@gmail.com>
2021-09-02 20:20:26 -0500

    initial commit for container_manager

---
+++ container_manager.py
@@ -0,0 +1,21 @@
+
+# Object to keep Customers for a container organized
+# Object to  generate reports on a container
+
+"""
+ContainerManager
+.get_known_customers
+.get_unknown_customers
+.get_customers
+.get_total_entries
+
+.load_knowns(complete_file)
+
+.generate_new_report(old_container_manager)
+.generate_gap_report(old_container_manager)
+.generate_unknowns()
+.generate_knowns()
+"""
+class ContainerManager:
+    def __init__(self, known_file=None):
+        pass
