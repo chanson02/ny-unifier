7302036acee7e08fad047465936efded51ecabb9
Cooper Hanson <hancooj@dunwoody.edu>
2023-06-09 08:43:09 -0500

    Can upload reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,28 @@
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    upload = params[:report][:file]
+    @report = Report.new
+    @report.name = upload.original_filename
+    @report.file.attach(upload)
+
+    if @report.save
+      redirect_to reports_path, notice: "Uploaded #{@report.name}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:file)
+  end
+end
