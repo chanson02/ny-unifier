6d52751905f72e930a25b6ce0b0e3e00e8ba4229
Cooper Hanson <hancooj@dunwoody.edu>
2023-06-13 13:33:33 -0500

    Ignore months in header value

--- app/models/header.rb
+++ app/models/header.rb
@@ -2,9 +2,12 @@ class Header < ApplicationRecord
   belongs_to :instruction, optional: true
   has_many :reports
 
+  MONTHS = %w[january jan february feb march mar april apr may june jun july jul august aug september sep october oct november nov december dec].freeze
+
   # this is intended to be a row from a csv
   def self.clean(str)
     # replace all digits with |
-    str.to_s.strip.downcase.parameterize.gsub(/\d+/, '|')
+    # replace all months with |
+    str.to_s.strip.downcase.parameterize.gsub(/\d+/, '|').gsub(/\b(?:#{MONTHS.join('|')})\b/, '|')
   end
 end
