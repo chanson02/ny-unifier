2897e2740b04449dec4b10fc60aeb3921c940822
Cooper Hanson <hancooj@dunwoody.edu>
2023-06-14 16:21:10 -0500

    Fixed a bug with getting the brand out of instructions

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -12,7 +12,7 @@ class Instruction < ApplicationRecord
     ins = Instruction.find_or_create_by(
       structure: params[:structure],
       retailer: params[:retailer],
-      brand: [params[:brand&.to_i]],
+      brand: [params[:brand]&.to_i],
       address: address,
       phone: params[:phone],
       website: params[:website],
